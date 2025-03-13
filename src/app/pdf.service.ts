5841212748813e9a52049157b007610aa1a60399
Dissoares <diegosoaresyt@gmail.com>
2025-03-13 01:06:01 -0300

    adicionando métod para gerar colunas dinamicas

--- src/app/pdf.service.ts
+++ src/app/pdf.service.ts
@@ -5,9 +5,69 @@ import { jsPDF } from 'jspdf';
 @Injectable({
   providedIn: 'root',
 })
-
 export class PdfService {
   constructor() {}
+
+  // public gerarPdf(
+  //   dadosArray: any[],
+  //   textoCabecalho: string,
+  //   textoRodape: string,
+  //   logoUrl: string
+  // ) {
+  //   const doc = new jsPDF();
+
+  //   doc.addImage(logoUrl, 'PNG', 10, 10, 30, 30);
+  //   doc.setFontSize(18);
+  //   doc.text(textoCabecalho, 50, 20);
+
+  //   let startY = 40;
+
+  //   dadosArray.forEach((item) => {
+  //     item.pessoa.forEach((pessoa: any, index: any) => {
+  //       autoTable(doc, {
+  //         startY,
+  //         body: [
+  //           [
+  //             `Nome: ${pessoa.name}`,
+  //             `Idade: ${pessoa.idade}`,
+  //             `Email: ${pessoa.email}`,
+  //             `Rua: ${item.endereco[index]?.rua || 'N/A'}`,
+  //             `Número: ${item.endereco[index]?.numero || 'N/A'}`,
+  //             `Número: ${item.endereco[index]?.numero || 'N/A'}`,
+  //             `Número: ${item.endereco[index]?.numero || 'N/A'}`,
+  //           ],
+  //           [
+  //             `Cargo: ${item.vinculos[index]?.cargo || 'N/A'}`,
+  //             `Salário: ${item.vinculos[index]?.salario || 'N/A'}`,
+  //             `Salário: ${item.vinculos[index]?.salario || 'N/A'}`,
+  //             `Salário: ${item.vinculos[index]?.salario || 'N/A'}`,
+  //             `Salário: ${item.vinculos[index]?.salario || 'N/A'}`,
+  //             `Salário: ${item.vinculos[index]?.salario || 'N/A'}`,
+  //             `Salário: ${item.vinculos[index]?.salario || 'N/A'}`,
+  //             `Número: ${item.endereco[index]?.numero || 'N/A'}`,
+  //           ],
+  //         ],
+  //         theme: 'grid',
+  //       });
+
+  //       startY = (doc as any).lastAutoTable.finalY + 10;
+  //     });
+  //   });
+
+  //   const contadorDePaginas = doc.getNumberOfPages();
+  //   for (let i = 1; i <= contadorDePaginas; i++) {
+  //     doc.setPage(i);
+  //     doc.setFontSize(10);
+  //     doc.text(textoRodape, 10, doc.internal.pageSize.height - 10);
+  //     doc.text(
+  //       `Página ${i} de ${contadorDePaginas}`,
+  //       doc.internal.pageSize.width - 50,
+  //       doc.internal.pageSize.height - 10
+  //     );
+  //   }
+
+  //   doc.save('relatorio-personalizado.pdf');
+  // }
 
   public gerarPdf(
     dadosArray: any[],
@@ -33,7 +93,7 @@ export class PdfService {
           );
 
           const corpoTabela: CellInput[][] = dadosTabela.map(
-            (obj) => Object.values(obj) as CellInput[] 
+            (obj) => Object.values(obj) as CellInput[]
           );
 
           autoTable(doc, {
